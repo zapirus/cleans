3b4979707b9cbc15d506e0578c6f750a3ed54a13
Запир <ibragimov-009@mail.ru>
2023-12-19 06:18:56 +0300

    refac

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jackc/pgx/v4"
 
+	"clean/handlers"
 	"clean/pkg/types"
 )
 
@@ -32,9 +33,7 @@ func NewPostgres(cfg *Configs) *Postgres {
 
 func (p *Postgres) Start(ctx context.Context) error {
 	fmt.Println("start postgres")
-
 	connDB := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", p.cfg.User, p.cfg.Password, p.cfg.Host, p.cfg.Port, p.cfg.DBName, p.cfg.SSLMode)
-
 	conn, err := pgx.Connect(ctx, connDB)
 	if err != nil {
 		return err
@@ -52,60 +51,29 @@ func (p *Postgres) Shutdown(ctx context.Context) error {
 	return nil
 }
 
-func (p *Postgres) Login(ctx context.Context, login, password string) (*string, error) {
+func (p *Postgres) CreateUser(ctx context.Context, user *handlers.RequestUser) (*string, error) {
 	var userID string
-	query := "SELECT login FROM users WHERE login = $1 AND password = $2 LIMIT 1"
-	err := p.conn.QueryRow(ctx, query, login, password).Scan(&userID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, fmt.Errorf("user not found")
-		}
+	query := "INSERT INTO users (guid, login, password, name, email) VALUES ($1, $2, $3, $4, $5) RETURNING guid"
+
+	if err := p.conn.QueryRow(ctx, query, user.Guid, user.Login, user.Password, user.Name, user.Email).Scan(&userID); err != nil {
 		return nil, err
 	}
 	return &userID, nil
+
 }
 
-func (p *Postgres) Register(ctx context.Context, user *types.User) (*string, error) {
-	var userID string
-	val := p.checkLogin(ctx, user.Login)
+func (p *Postgres) FindUser(ctx context.Context, val map[string]string) (*types.User, error) {
+	var user types.User
 
-	if val {
-		return &userID, fmt.Errorf("пользователь с логином - %s уже существует. Попробуйте другой логин", user.Login)
-	}
+	query := "SELECT login, password, name, email FROM users WHERE login = $1"
 
-	query := "INSERT INTO users (login, password, name, email) VALUES ($1, $2, $3, $4) RETURNING login"
-
-	err := p.conn.QueryRow(ctx, query, user.Login, user.Password, user.Name, user.Email).Scan(&userID)
-	if err != nil {
-		return nil, err
-	}
-
-	return &userID, nil
-}
-
-func (p *Postgres) Reset(ctx context.Context, login string) (*string, error) {
-	var userID string
-	query := "SELECT email FROM users WHERE login = $1  LIMIT 1"
-	err := p.conn.QueryRow(ctx, query, login).Scan(&userID)
+	err := p.conn.QueryRow(ctx, query, val["login"]).Scan(&user.Login, &user.Password, &user.Name, &user.Email)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return nil, fmt.Errorf("user not found")
+			return nil, errors.New("user not found")
 		}
 		return nil, err
 	}
 
-	return &userID, nil
-}
-
-func (p *Postgres) Verify(ctx context.Context, mail, verifyCode string) (string, error) {
-	login := ""
-	password := ""
-
-	query := "SELECT login, password FROM users WHERE email = $1"
-	err := p.conn.QueryRow(ctx, query, mail).Scan(&login, &password)
-
-	if errors.Is(err, sql.ErrNoRows) {
-		return "", fmt.Errorf("user not found")
-	}
-	return "Ваш логин: " + login + " " + "Ваш пароль: " + password, err
+	return &user, nil
 }
--- pkg/types/user.go
+++ pkg/types/user.go
@@ -1,6 +1,12 @@
 package types
 
+import (
+	"github.com/google/uuid"
+	_ "github.com/google/uuid"
+)
+
 type User struct {
+	Guid       uuid.UUID
 	Login      string
 	Password   string
 	Name       string
